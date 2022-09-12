7409f010c7a78ee709e6eee7d0ff5e2b774a186c
rmsdnjs518 <rmsdnjs518@gmail.com>
2022-09-12 10:22:37 +0900

    new py algorithm practice

---
+++ py_stack/stack.py
@@ -0,0 +1,25 @@
+
+def main():
+	num = input()
+	
+	for i in range(num):
+		
+	
+	
+def push():
+    	
+def pop():
+    	
+def size():
+    	
+def empty():
+    
+
+def top():
+    	
+	
+	
+		
+	
+if __name__ == "__main__":
+	main()
